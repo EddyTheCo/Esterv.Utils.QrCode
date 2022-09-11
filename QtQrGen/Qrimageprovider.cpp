6e9b4c60b7af9acdf6991dcaad75f3251f59ecb7
EddyGlezz <eddyglezz95@gmail.com>
2022-09-11 11:04:36 +0200

    set background transparent

--- QtQrGen/Qrimageprovider.cpp
+++ QtQrGen/Qrimageprovider.cpp
@@ -8,23 +8,23 @@ using namespace qrcodegen;
 
 QPixmap QRImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    const int width = 100;
+	const int width = 100;
 
-    const auto max=(requestedSize.width()>requestedSize.height())?requestedSize.width():requestedSize.height();
+	const auto max=(requestedSize.width()>requestedSize.height())?requestedSize.width():requestedSize.height();
 
-    QPixmap pixmap(max > 0 ? max : width,
-                   max> 0 ? max : width);
+	QPixmap pixmap(max > 0 ? max : width,
+			max> 0 ? max : width);
+	pixmap.fill( Qt::transparent );
+	*size = pixmap.size();
+	const QrCode qr = QrCode::encodeText(id.toStdString().c_str(), static_cast<QrCode::Ecc>(errC));
+	const auto qrSVGstr=toSvgString(qr, color.name().toStdString());
 
-     *size = pixmap.size();
-    const QrCode qr = QrCode::encodeText(id.toStdString().c_str(), static_cast<QrCode::Ecc>(errC));
-    const auto qrSVGstr=toSvgString(qr, color.name().toStdString());
+	auto qrImage=QSvgRenderer(QByteArray::fromStdString(qrSVGstr));
+	QPainter Painter;
 
-    auto qrImage=QSvgRenderer(QByteArray::fromStdString(qrSVGstr));
-    QPainter Painter;
-
-    Painter.begin(&pixmap);
-    qrImage.render(&Painter);
-    Painter.end();
-    return pixmap;
+	Painter.begin(&pixmap);
+	qrImage.render(&Painter);
+	Painter.end();
+	return pixmap;
 }
 
