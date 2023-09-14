6c50ee27eff441a93cd48109026f5b04f766ae62
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-09-14 14:29:34 +0200

    added the emscriptem macro and wasm functions

--- QtQrDec/Qrimagedecoder.cpp
+++ QtQrDec/Qrimagedecoder.cpp
@@ -3,12 +3,57 @@
 #include <QQuickImageProvider>
 #include <QDebug>
 #include<QImage>
-#ifdef USE_EMSCRIPTEN
-#include <emscripten/bind.h>
-#endif
+
 using namespace qrcodedec;
 
+
+namespace fooQtQrDec
+{
+QString fooPrint(void)
+{
+    return "fooPrint";
+}
+}
+#ifdef USE_EMSCRIPTEN
+#include <emscripten.h>
+#include <emscripten/bind.h>
 QRImageDecoder* QRImageDecoder::m_decoder=nullptr;
+
+EM_JS(void, call_start, (), {
+    let video = document.querySelector("#qrvideo");
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
+    let canvas = document.querySelector("#qrcanvas");
+    getimage=setInterval(function() {
+            let imageData = canvas.getContext('2d').getImageData(0, 0, video.width, video.height);
+            var data=imageData.data.buffer;
+            var uint8Arr = new Uint8Array(data);
+            if (qtLoader.module()) {
+                qtLoader.module().QRImageDecoder.getdecoder().set_data(uint8Arr,video.width,video.height);
+            }
+        }, 500);
+    $("#qrDecoder").toggle();
+});
+EM_JS(void, call_stop, (), {
+      clearInterval(getimage);
+    $("#qrDecoder").toggle();
+});
+EMSCRIPTEN_BINDINGS(qrdecoder) {
+    emscripten::class_<QRImageDecoder>("QRImageDecoder")
+        .function("set_data", &QRImageDecoder::set_data)
+    .class_function("get_editor", &QRImageDecoder::getdecoder, emscripten::allow_raw_pointers());
+
+}
+void QRImageDecoder::start()const
+{
+    call_start();
+}
+void QRImageDecoder::stop()const
+{
+    call_stop();
+}
+#endif
+
 void QRImageDecoder::set_source(const QString & path )
 {
     if(path=="") return;
@@ -18,15 +63,13 @@ void QRImageDecoder::set_source(const QString & path )
         return;
 
     QUrl imageUrl(path);
-    qDebug()<<imageUrl;
     auto provider = reinterpret_cast<QQuickImageProvider*>( myQmlEngine->imageProvider(imageUrl.host()));
 
     if (provider->imageType()==QQuickImageProvider::Image){
         picture = provider->requestImage(imageUrl.path().remove(0,1),nullptr,QSize());
 
     }
-    decodePicture(picture);
-
+    if(!picture.isNull())decodePicture(picture);
 
 }
 void QRImageDecoder::decodePicture(QImage& picture)
@@ -45,9 +88,3 @@ void QRImageDecoder::set_data(unsigned char* img,int rows ,int cols)
     QImage picture(img,cols,rows,QImage::Format_RGBA8888);
     decodePicture(picture);
 }
-#ifdef USE_EMSCRIPTEN
-EMSCRIPTEN_BINDINGS(qrdecoder) {
-    emscripten::class_<QRImageDecoder>("QRImageDecoder")
-        .function("set_data", &QRImageDecoder::set_data);
-}
-#endif
--- QtQrDec/include/Qrimagedecoder.hpp
+++ QtQrDec/include/Qrimagedecoder.hpp
@@ -2,17 +2,28 @@
 #include<QString>
 #include <QtQml/qqmlregistration.h>
 
+//foo namespace to force the linker to link the backing library composed only of qml files
+namespace fooQtQrDec
+{
+QString fooPrint(void);
+};
 class QRImageDecoder : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString text READ get_text NOTIFY text_changed)
     Q_PROPERTY(QString source READ get_source WRITE set_source NOTIFY source_changed)
+
     QML_ELEMENT
+    QML_SINGLETON
 
 public:
-    QRImageDecoder(){
-        m_decoder=this;
+    QRImageDecoder()
+    {
     };
+#ifdef USE_EMSCRIPTEN
+    Q_INVOKABLE void start()const;
+    Q_INVOKABLE void stop()const;
+#endif
     QString get_text(void)
     {
         return text;
@@ -21,16 +32,22 @@ public:
     {
         return source;
     }
+#ifdef USE_EMSCRIPTEN
     static QRImageDecoder* getdecoder(){return m_decoder;};
+#endif
     void set_source(const QString &file );
     void set_data(unsigned char* img,int rows ,int cols);
 signals:
     void text_changed();
     void source_changed();
+
 private:
+#ifdef USE_EMSCRIPTEN
+    static QRImageDecoder* m_decoder;
+#endif
     void decodePicture(QImage& picture);
     QString text,source;
-    static QRImageDecoder* m_decoder;
+
 };
 
 
