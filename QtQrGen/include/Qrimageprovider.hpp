293c3b3b8b5b409d31e71dfd8e239c4aebff7b30
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-03-15 10:50:02 +0100

    Fixes (#76)
    * use develop branch for docs
    
    * added explicit export for windows shared libs

--- QtQrGen/include/Qrimageprovider.hpp
+++ QtQrGen/include/Qrimageprovider.hpp
@@ -1,7 +1,13 @@
 #include <qquickimageprovider.h>
 
+#include <QtCore/QtGlobal>
+#if defined(WINDOWS_GEN)
+# define GEN_EXPORT Q_DECL_EXPORT
+#else
+#define GEN_EXPORT Q_DECL_IMPORT
+#endif
 
-class QRImageProvider : public QQuickImageProvider
+class GEN_EXPORT QRImageProvider : public QQuickImageProvider
 {
 public:
     QRImageProvider(int erc=0)
